13cbbf65d7d431502a45fb3a7fc72f4c93d77193
Jeremy Elman <jelman@lbl.gov>
2013-11-14 13:18:32 -0800

    Changes to column names on featquery import import and made legend semi-transparent

--- CreateFeatquery_2ndLevel.py
+++ CreateFeatquery_2ndLevel.py
@@ -6,15 +6,9 @@ import matplotlib.pyplot as plt
 
 def parse_report(fqdir, statnames):
     infile = os.path.join(fqdir, 'report.txt')
-    fqreport = pd.read_csv(infile, header=None,delimiter='\s')
-    fqreport.pop('X0')
-    mycols = collections.OrderedDict([('X1', 'stat'), ('X2', 'numvoxels'), 
-    ('X3', 'min'), ('X4', '10pct'), ('X5', 'mean'), ('X6', 'median'), 
-    ('X7', '90pct'), ('X8', 'max'), ('X9', 'stddev'), ('X10', 'voxX'), 
-    ('X11', 'voxY'), ('X12', 'voxZ'), ('X13', 'mmX'), ('X14', 'mmY'), 
-    ('X0', 'stat'), ('X15', 'mmZ')])
-    fqreport_rename = fqreport.rename(columns=mycols)
-    fqreport_rename = fqreport_rename.set_index('stat')
+    fqreport = pd.read_csv(infile, header=None,delimiter='\s',names=['nSubs','stat','nVoxels','min','10pct',
+    'mean','median','90pct','max','stdev','voxX','voxY','voxZ','mmX','mmY','mmZ'])
+    fqreport_rename = fqreport.set_index('stat')
     zreport = fqreport_rename.ix[['zstat' in x for x in fqreport_rename.index]]
     zreport = zreport.rename(index=
                                 collections.OrderedDict(zip(zreport.index, stat_names)))
@@ -40,9 +34,8 @@ if __name__ == '__main__':
     outdir = '/home/jagust/DST/FSL/results'
     stat_names = ['Details1', 'Details2', 'Details3', 'Details4', 'Details5']
     # Specify group membership if available, otherwise set to None
-    groupinfo = pd.read_csv('/home/jagust/DST/Included_Subjects.csv', sep=None, index_col=0)
-    groupinfo = groupinfo['GROUP']
-    groupinfo = groupinfo.str.replace('old.*', 'old')
+    groupinfo = pd.read_csv('/home/jagust/DST/FSL/spreadsheets/Included_Subjects.csv', sep=None, index_col=0)
+    groupinfo = groupinfo['Status']
     outname = os.path.join(outdir, fqdirname.strip('featquery_'))
     
     # Create empty dataframes to hold full results
@@ -52,6 +45,7 @@ if __name__ == '__main__':
     copemaxdf = pd.DataFrame(index=stat_names)
     
     for subj in sublist:
+        print 'Starting on subject %s'%(subj)
         globstr = os.path.join(basedir, 
                                 '2ndLevel/Details',
                                 subj + '.gfeat',
@@ -70,27 +64,27 @@ if __name__ == '__main__':
         
     if not zmeandf.empty:
         zmeandfT = zmeandf.T
-        zmeandfT.insert(0, 'Group', groupinfo)
+        zmeandfT.insert(0, 'Status', groupinfo)
         zmeandfT.to_csv(outname + '_zmean.csv', sep='\t', index=True, index_label='Subject')
-        zmean_grp = zmeandfT.groupby(by='Group')            
+        zmean_grp = zmeandfT.groupby(by='Status')            
      
     if not zmaxdf.empty:            
         zmaxdfT = zmaxdf.T
-        zmaxdfT.insert(0, 'Group', groupinfo)
+        zmaxdfT.insert(0, 'Status', groupinfo)
         zmaxdfT.to_csv(outname + '_zmax.csv', sep='\t', index=True, index_label='Subject')
-        zmax_grp = zmaxdfT.groupby(by='Group')
+        zmax_grp = zmaxdfT.groupby(by='Status')
    
     if not copemeandf.empty:
         copemeandfT = copemeandf.T
-        copemeandfT.insert(0, 'Group', groupinfo)
+        copemeandfT.insert(0, 'Status', groupinfo)
         copemeandfT.to_csv(outname + '_copemean.csv', sep='\t', index=True, index_label='Subject')
-        copemean_grp = copemeandfT.groupby(by='Group')
+        copemean_grp = copemeandfT.groupby(by='Status')
      
     if not copemaxdf.empty:    
         copemaxdfT = copemaxdf.T
-        copemaxdfT.insert(0, 'Group', groupinfo)
+        copemaxdfT.insert(0, 'Status', groupinfo)
         copemaxdfT.to_csv(outname + '_copemax.csv', sep='\t', index=True, index_label='Subject')
-        copemax_grp = copemaxdfT.groupby(by='Group')
+        copemax_grp = copemaxdfT.groupby(by='Status')
 
         
     fig, axes = plt.subplots(nrows=2, ncols=2)
@@ -98,6 +92,7 @@ if __name__ == '__main__':
     zmax_grp.mean().T.plot(ax=axes[0,1], title='Z max', marker='o', legend=False)
     copemean_grp.mean().T.plot(ax=axes[1,0], title='Cope Mean', marker='o', legend=False)
     copemax_grp.mean().T.plot(ax=axes[1,1], title='Cope Max', marker='o')
-    plt.legend(bbox_to_anchor=(0, 0, 1, 1), bbox_transform=plt.gcf().transFigure)
+    leg = plt.legend(bbox_to_anchor=(0, 0, 1, 1), bbox_transform=plt.gcf().transFigure, fancybox=True)
+    leg.get_frame().set_alpha(0.7)
     plt.tight_layout()
     plt.savefig(outname + '.png')
